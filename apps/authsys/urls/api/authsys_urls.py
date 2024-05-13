8d88e87ab305f068cb445fb76a9488438bcba756
Umar Farooq <umar.wzbd@gmail.com>
2024-05-13 21:49:01 +0500

    s

--- apps/authsys/urls/api/authsys_urls.py
+++ apps/authsys/urls/api/authsys_urls.py
@@ -1,25 +1,17 @@
 # PACKAGES
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
 # VIEWS
 from apps.authsys.views.api import user_view
 from apps.authsys.views.api import authn_view
 
-router = DefaultRouter()
-# router.register("", user_view.UserViewSet)
 urlpatterns = [
-    # path("/account/", include(router.urls)),
-    # path(
-    #     "/account",
-    #     UserViewSet.get_account({"get": "get_account"}),
-    # ),
-    # ACCOUNT
-    path("/account/", user_view.user_create, name="create-account"),
-    path("/account/get/", user_view.user_delete, name="get-account"),
+    # USER MANAGEMENT (CRUD)
+    path("/account/create/", user_view.user_create, name="create-account"),
+    path("/account/get/", user_view.user_get, name="get-account"),
     path("/account/update/", user_view.user_update, name="update-account"),
-    path("/account/delete/", user_view.user_create, name="delete-account"),
-    # Authentication
+    path("/account/delete/", user_view.user_delete, name="delete-account"),
+    # AUTHENTICATION
     path("/authn/login/", authn_view.Login.as_view(), name="token_obtain_pair"),
     path("/authn/refresh", authn_view.TokenRefresh.as_view(), name="token_refresh"),
     path("/authn/csrf/", authn_view.get_csrf_token),
--- apps/authsys/views/api/user_view.py
+++ apps/authsys/views/api/user_view.py
@@ -8,20 +8,16 @@ from apps.authsys.models.user_model import User
 
 from apps.authsys.serializers.user_serializer import UserSerializer
 
-"""
-Making use of GenericViewSet, because only needed user creation (registration) and updation, and also custom  methods. No need for users list, and retrieve single user by id. So that's the perfect use case of using  GenericViewSet, of course URLs for custom methods will be automatically generated as well, based on thier names. So that's the benefit we get with viewsets.
-
-Also I needed to user create_user method for creation, that was also the reason to to customize create method. 
-"""
-
 
 @api_view(["POST"])
 def user_create(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        user = User.objects.create_user(**serializer.validated_data)
-        user_serializer = UserSerializer(user)
-        return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        User.objects.create_user(**serializer.validated_data)
+        return Response(
+            "Your registration has been successful. Please verify your email.",
+            status=status.HTTP_201_CREATED,
+        )
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
